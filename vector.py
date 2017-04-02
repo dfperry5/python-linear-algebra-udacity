faa4ba6646dd0571e12aa33848da3ca4102d8c1b
Dylan Perry <dylanperry@dylans-mbp.attlocal.net>
2017-04-02 15:11:45 -0400

    commiting vector file -- first lesson

---
+++ vector.py
@@ -0,0 +1,33 @@
+class Vector(object):
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+    #Allows Pythons built in print function to work on class
+    def __str__(self):
+        return ' Vector: {}'.format(self.coordinates)
+
+    # Allows Python equality checks.
+    def __eq__(self,v):
+        return self.coordinates == v.coordinates
+
+#Lesson 1 -- 
+my_vector = Vector([1,2,3])
+print (my_vector)
+my_vector_2 = Vector([1,2,3])
+my_vector_3 = Vector([-1,2,3])
+
+print (my_vector == my_vector_2)
+print (my_vector_2 == my_vector_3)
+
+
+            
+    
